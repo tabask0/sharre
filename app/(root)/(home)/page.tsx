c6faa32244339e1aa563476f744a28c0096f3712
Andrei Bucur <andreibucur@Andreis-MacBook-Pro.local>
2024-08-17 19:15:27 +0300

    finished project

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,17 +1,40 @@
-"use client"
+"use client";
 
-import React from "react"
+import MeetingTypeList from "@/components/MeetingTypeList";
+import React from "react";
 
 const Home = () => {
+  const now = new Date();
+
+  const time = now.toLocaleTimeString("en-RO", {
+    hour12: false,
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  const date = now.toLocaleDateString("en-RO", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    weekday: "long",
+  });
+
   return (
-    <section className='flex size-full flex-col gap-10 text-white'>
-      <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
-        <div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p11'>
-          <h2>Upcoming meeting at: </h2>
+    <section className="flex size-full flex-col gap-10 text-white">
+      <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
+        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
+          <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">
+            Upcoming meeting at: 14:15
+          </h2>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
+            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
+          </div>
         </div>
       </div>
-    </section>
-  )
-}
 
-export default Home
+      <MeetingTypeList />
+    </section>
+  );
+};
+
+export default Home;
